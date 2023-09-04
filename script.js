ffccf9be7a4a7856009318d7a47a2140c90a1844
spencerhenegar-Zwufst <spencerhenegar@gmail.com>
2023-09-04 16:27:35 -0700

    added the function of if someone selects no to all questions it will loop them through prompts again to see which option they want

--- script.js
+++ script.js
@@ -1,12 +1,19 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
+
+// input values
 var characterLength;
+var confirmSymbols;
+var confirmNumbers;
+var confirmUppercase;
+var confirmLowercase;
 
- var lettersLowercase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
- var lettersUppercase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+
+//typers of inputs/characters that can be included in the password
+ var alphaLowercase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+ var alphaUppercase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
  var numbers = ["0","1","2","3","4","5","6","7","8","9"]
-  var specialSymbols = ["!", "%", "&", ",", "*", "+", "-", ".", "/", "<", ">", "?","~"];
-
+var specialSymbols = ["!", "%", "&", ",", "*", "+", "-", ".", "/", "<", ">", "?","~"];
 
 // Write password to the #password input
 function writePassword() {
@@ -30,16 +37,17 @@ characterLength = (prompt("Choose the amount of characters between 8 - 128 your 
     confirm("Select okay if you are satisfied with the characters you put in?");
   }
 
-confirm("Select okay if you want lowercase letters in your password?");
+var confirmLowercase = confirm("Select okay if you want lowercase letters in your password?");
+var confirmUppercase = confirm("Select okay if you want uppercase letters in your password?");
+var confirmNumbers = confirm("Select okay if you want numbers in your password?");
+var confirmSymbols = confirm("Select okay if you want special characters letters in your password?");
 
-confirm("Select okay if you want uppercase letters in your password?");
-
-confirm("Select okay if you want numbers in your password?");
-
-confirm("Select okay if you want special characters letters in your password?");
+if (confirmLowercase == false & confirmNumbers == false & confirmSymbols == false & confirmUppercase == false) {
+  alert("In order to generate a password you must select one of the following options.");
 }
 
 
+}
 
 
 
